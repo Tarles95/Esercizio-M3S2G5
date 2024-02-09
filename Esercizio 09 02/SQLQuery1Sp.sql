0ed1bb6cf2b7e0f85b1b79f675ff616e353c6706
Tarles95 <148482049+Tarles95@users.noreply.github.com>
2024-02-09 16:43:49 +0100

    Eserccizio 09 02 2024

---
+++ Esercizio 09 02/SQLQuery1Sp.sql
@@ -0,0 +1,8 @@
+--CREATE PROCEDURE AnnoCont
+--@Anno INT 
+--AS
+--BEGIN
+--	select * from Verbale
+--	where YEAR(Data_Trascrizione_Verbale) = @Anno    	
+--END
+
---
+++ Esercizio 09 02/SQLQuery1Sp2.sql
@@ -0,0 +1,8 @@
+--CREATE PROCEDURE SommaPunti1
+--@data date
+--AS
+--BEGIN
+--	select sum(decurtamentopunti) as SommaPunti from Verbale
+--	where data_trascrizione_verbale = @data  
+--END
+
